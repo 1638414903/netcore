9bd4f8e32b11edf9606b54b626eea262f9d040d6
lwl <you@example.com>
2020-06-10 16:35:33 +0800

    更改

--- mvccore/Controllers/HomeController.cs
+++ mvccore/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace mvccore.Controllers
         {
             return View();
         }
+        public IActionResult ceshi()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
